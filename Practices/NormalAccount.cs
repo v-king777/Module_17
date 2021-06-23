0e2de3f6fbd9a976b350b08844451a1e73ec7f3d
v-king777 <v.kolpashchikov@gmail.com>
2021-06-23 11:27:13 +0300

    modified:   Practices/NormalAccount.cs

--- Practices/NormalAccount.cs
+++ Practices/NormalAccount.cs
@@ -4,9 +4,24 @@ namespace Practices
 {
     internal class NormalAccount : IAccount
     {
-        public void CalculateInterest()
+        public void CalculateInterest(double balance)
         {
-            throw new NotImplementedException();
+            double interest;
+
+            interest = balance * 0.4;
+
+            if (balance < 1000)
+            {
+                interest -= balance * 0.2;
+            }
+
+            if (balance < 50000)
+            {
+                interest -= balance * 0.4;
+            }
+
+            // Странные расчёты. Что бы они не значили, они взяты из задания
+            Console.WriteLine($"Процентная ставка для обычного аккаунта: {interest}");
         }
     }
 }
