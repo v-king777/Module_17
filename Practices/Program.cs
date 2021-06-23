0a459619a5aee7865d639e607717f1be810de920
v-king777 <v.kolpashchikov@gmail.com>
2021-06-23 11:27:27 +0300

    modified:   Practices/Program.cs

--- Practices/Program.cs
+++ Practices/Program.cs
@@ -1,11 +1,14 @@
-﻿using System;
-
-namespace Practices
+﻿namespace Practices
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            IAccount account = new NormalAccount(); // Создаём аккаунт нужного типа
+            Calculator calculator = new Calculator(account); // Загоняем его в калькулятор
+
+            calculator.Balance = 100000;
+            calculator.Calculate();
         }
     }
 }
